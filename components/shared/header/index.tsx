8b2d4bca58f34047c0c924aa77a12a1a9c199028
Hamza11067 <hamzakhalid1067@gmail.com>
2026-06-05 11:35:40 +0500

    setting up light and dark theme

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,8 +1,9 @@
 import { ShoppingCart, UserIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./Mode-toggle";
 
 const Header = () => {
   return (
@@ -17,21 +18,23 @@ const Header = () => {
               height={48}
               priority={true}
             />
-            <span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
           </Link>
         </div>
         <div className="space-x-2">
-            <Button asChild variant="ghost">
-                <Link href="/cart">
-                    <ShoppingCart /> Cart
-                </Link>
-            </Button>
-            <Button asChild variant="ghost">
-                <Link href="/signin">
-                    <UserIcon /> Sign In
-                </Link>
-            </Button>
-         
+          <ModeToggle />
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant="default" className="rounded-md">
+            <Link href="/signin">
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
         </div>
       </div>
     </header>
